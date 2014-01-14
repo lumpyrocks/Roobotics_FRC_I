7efb1bb4c99808e4ed8ca09a359517fb309d651c
lumpyrocks <99lumpyrocks@gmail.com>
2014-01-14 17:35:40 -0500

    Victor in Port 1 at 100
    Test Code 1

--- src/edu/wpi/first/wpilibj/templates/Robot.java
+++ src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -9,7 +9,7 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-
+import edu.wpi.first.wpilibj.Victor;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -18,12 +18,14 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot extends IterativeRobot {
+    Victor victor = new Victor (1);
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-
+        
     }
 
     /**
@@ -37,7 +39,7 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+        victor.set (1);
     }
     
     /**
