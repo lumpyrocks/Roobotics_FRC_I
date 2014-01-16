5a3834c948bb2de919a0437d79d0acbb1ce66203
735tesla <henryjamespit@gmail.com>
2014-01-16 16:31:22 -0500

    Added RooTalon class

---
+++ src/edu/wpi/first/wpilibj/templates/RooTalon.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.*;
+/**
+ *
+ * @author henrypitcairn
+ */
+public class RooTalon extends Talon {
+    private double speed;
+    public RooTalon(int channel) {
+        super(channel);
+    }
+    public RooTalon(int slot, int channel) {
+        super(slot, channel);
+    }
+    public double getSpeed() {
+        return speed;
+    } //
+    public void dontCallMe(){}
+    public void set(double speed) {
+        this.speed = speed;
+        super.set(speed);
+    }
+    
+}
