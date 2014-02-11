526d5c2a1b9efc88e7aa51bb2a42cd00616b5189
Dorian <15doriank@abingtonfriends.net>
2014-02-11 16:58:05 -0500

    Added Kicker -Maki

---
+++ src/edu/wpi/first/wpilibj/templates/RooKicker.java
@@ -0,0 +1,15 @@
+package edu.wpi.first.wpilibj.templates;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 15BenjaminH
+ */
+public class RooKicker {
+    
+}
