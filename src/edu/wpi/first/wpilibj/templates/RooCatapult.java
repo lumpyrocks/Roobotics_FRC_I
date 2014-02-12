4105b8202f8eb0ec12cc68e199c2866c42d9d009
Sialocin <anna.nicolais@gmail.com>
2014-02-12 17:17:31 -0500

    Added Catapult code :)

--- src/edu/wpi/first/wpilibj/templates/RooCatapult.java
+++ src/edu/wpi/first/wpilibj/templates/RooCatapult.java
@@ -10,6 +10,26 @@ package edu.wpi.first.wpilibj.templates;
  *
  * @author henrypitcairn
  */
-public class RooCatapult {
+public class RooCatapult{
+    private RooCatapultMotorPair motors;
     
+    public RooCatapult(){ 
+        motors = new RooCatapultMotorPair(false);
+    }
+    public void lauch(){
+        try{
+            for(double i = 0; i<=1.0; i+= 0.1){
+                Thread.sleep(3);
+                motors.setSpeed(i);
+            }
+            Thread.sleep(10);
+            for(double i = 1; i>=0; i = i - .1){
+                Thread.sleep(3);
+                motors.setSpeed(i);
+            }
+        } catch(java.lang.InterruptedException e){
+            
+        }
+        motors.setSpeed(0);
+    }
 }
