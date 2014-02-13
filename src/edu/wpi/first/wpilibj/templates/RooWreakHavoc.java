d3e89b22c72fd096d8979315e94a48ddb6cf9cb2
735tesla <henryjamespit@gmail.com>
2014-02-12 20:07:36 -0500

    Added a class that will become very important later

---
+++ src/edu/wpi/first/wpilibj/templates/RooWreakHavoc.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author henrypitcairn
+ */
+public class RooWreakHavoc {
+    public void killEveryone() {
+    
+    }
+    public void randomForkLift() {
+        
+    }
+    public void spinInCircles() {
+        
+    }
+    public void fullSteamAhead() {
+        
+    }
+    public void invertControls() {
+        
+    }
+    public void lockOnFullPower() {
+        
+    }
+}
