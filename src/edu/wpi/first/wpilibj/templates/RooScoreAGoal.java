20ac3824c499a986d9dea4f6feb63d14c4983bb4
Sialocin <anna.nicolais@gmail.com>
2014-02-17 14:59:43 -0500

    Scoring Stuffffs

--- src/edu/wpi/first/wpilibj/templates/RooScoreAGoal.java
+++ src/edu/wpi/first/wpilibj/templates/RooScoreAGoal.java
@@ -25,12 +25,30 @@ public class RooScoreAGoal {
     private RooForkLift fl;
     private RooCatapult pult;
     private RooDriveTrain rdt;
+    private boolean buttonHeldNow;
+    private boolean buttonHeldLastIteration;
     
     private RooScoreAGoal (){
         kicker = RooKicker.getInstance();
         fl = RooForkLift.getInstance();
         pult = RooCatapult.getInstance();
         rdt = RooDriveTrain.getInstance();
+        SmartDashboard.putNumber("Backup Stop Time", 500);
+        SmartDashboard.putNumber("Forklift Down Stop Time", 500);
+        SmartDashboard.putNumber("Kicker Up Stop Time",500);
+        SmartDashboard.putNumber("Kicker Down Stop Time",500);
+        SmartDashboard.putNumber("Forklift Up Stop Time",500);
+        buttonHeldNow = false;
+        buttonHeldLastIteration = false;
+    }
+    
+    public void periodic(){
+        if (buttonHeldNow == true && buttonHeldLastIteration == false){
+            scoreALowGoal();
+        }
+        buttonHeldLastIteration = buttonHeldNow;
+        buttonHeldNow = joystick.getRawButton(RobotMap.LAUNCH_BUTTON);
+        
     }
     public void scoreALowGoal(){
         try{
@@ -44,8 +62,6 @@ public class RooScoreAGoal {
             
             kicker.setSpeed(1.0);
             Thread.sleep((long)SmartDashboard.getNumber("Kicker Up Stop Time"));
-            kicker.setSpeed(0.0);
-            
             kicker.setSpeed(-1.0);
             Thread.sleep((long)SmartDashboard.getNumber("Kicker Down Stop Time"));
             kicker.setSpeed(0);
