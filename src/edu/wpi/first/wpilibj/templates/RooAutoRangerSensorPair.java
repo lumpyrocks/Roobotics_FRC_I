182f5b7ca4998f41ff3b5f092579e9602cf4122e
735tesla <henryjamespit@gmail.com>
2014-02-26 16:44:55 -0500

    SexyAnimal

--- src/edu/wpi/first/wpilibj/templates/RooAutoRangerSensorPair.java
+++ src/edu/wpi/first/wpilibj/templates/RooAutoRangerSensorPair.java
@@ -15,6 +15,10 @@ public class RooAutoRangerSensorPair implements Runnable {
     private static final double VOLTS_PER_INCH = 0.009766;
     private AnalogChannel sensor_right;
     private AnalogChannel sensor_left;
+    private SexyAnimal getStuff;
+    private double leftDistance;
+    private double rightDistance;
+    
     private static RooAutoRangerSensorPair rarsp = null;
     public static RooAutoRangerSensorPair getInstance() {
         if (rarsp == null) {
@@ -23,6 +27,7 @@ public class RooAutoRangerSensorPair implements Runnable {
         return rarsp;
     }
     public RooAutoRangerSensorPair() {
+        getStuff = new SexyAnimal();
         sensor_right = new AnalogChannel(RobotMap.ULTRASONIC_RANGER_CHANNE_RIGHT);
         sensor_left = new AnalogChannel(RobotMap.ULTRASONIC_RANGER_CHANNEL_LEFT);
         System.out.println("Sensors Created");
@@ -34,20 +39,27 @@ public class RooAutoRangerSensorPair implements Runnable {
         return inches;
     }
     
-    // Cook the numbers (if we ever want to)
-    private double cook(int orig) {
-        return orig;
-    }
     
     // Get the distance (in inches) from the left sensor
     public double getLeftDistance() {
-        return voltsToInches(sensor_left.getAverageVoltage());
+        updateLeftDistance();
+        updateBoth();
+        return leftDistance;
+    }
+    public void updateLeftDistance() {
+        leftDistance = voltsToInches(sensor_left.getAverageVoltage());
     }
     // Get the distance (in inches) from the right sensor
     public double getRightDistance() {
-        return voltsToInches(sensor_right.getAverageVoltage());
+        updateBoth();
+        return rightDistance;
     }
-    
+    public void updateRightDistance() {
+        rightDistance = voltsToInches(sensor_right.getAverageVoltage());
+    }
+    public void updateBoth() {
+        run();
+    }
     // Get the difference between the sensor readings (in inches)
     public double getDifference() {
         double difference = getLeftDistance()-getRightDistance();
@@ -60,6 +72,12 @@ public class RooAutoRangerSensorPair implements Runnable {
     }
 
     public void run() {
-        
+        updateRightDistance();
+        try {
+            Thread.sleep(300);
+        } catch (java.lang.InterruptedException e) {
+            
+        }
+        updateLeftDistance();
     }
 }
